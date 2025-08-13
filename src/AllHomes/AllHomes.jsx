922550a25692cee1ca7c64ee857de6e8912881c4
saiganesh-pitta <saiganeshpitta5@gmail.com>
2025-08-13 19:02:13 +0530

    Update AllHomes.jsx

--- src/AllHomes/AllHomes.jsx
+++ src/AllHomes/AllHomes.jsx
@@ -54,7 +54,7 @@ const AllHomes = () => {
               <div className="card-cont">
                 <img
                   className="card-img"
-                  src={`https://backend-two-jet-82.vercel.app/${items.photo}`}
+                  src={`${items.photo}`}
                 />
                 <div className="card-details">
                   <div className="card-header">
