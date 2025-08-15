88ff44e00edbcd4a3eab922c5c9a385fb9c1eddc
saiganesh-pitta <saiganeshpitta5@gmail.com>
2025-08-15 15:41:23 +0530

    Update Settings.jsx

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -105,7 +105,7 @@ const Settings = () => {
            {logData.user && <div onClick={handleLogOut} className="acc-cont">
                        <button className="acc-out">Log Out</button>
                </div>}
-           { !logData.user && <div onClick={navigate("/details/login")} className="acc-cont">
+           { !logData.user && <div onClick={()=>{navigate("/details/login")}} className="acc-cont">
                        <button className="acc-out">Log Out</button>
                </div>}
       </div>
